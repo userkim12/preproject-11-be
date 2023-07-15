e80ee14a07eed5b1ccd3a141d1afe3ee212813ad
jiooong <lucy991010@gmail.com>
2023-07-15 16:47:49 +0900

    Feat: Comment 엔티티 추가 및 Post 엔티티와의 연관관계 설정

--- src/main/java/com/example/music/comment/entity/Comment.java
+++ src/main/java/com/example/music/comment/entity/Comment.java
@@ -1,4 +1,33 @@
 package com.example.music.comment.entity;
 
+import com.example.music.post.entity.Post;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime modifiedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    /*
+    @ManyToOne
+    private User user;
+     */
+
 }
