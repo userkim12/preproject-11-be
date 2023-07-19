bb2659fde4064c4d32f1d8e06232b248db6a516f
userkim12 <potomq444@naver.com>
2023-07-19 17:09:26 +0900

    Fix: LoginRequestDto username valid 수정

--- src/main/java/com/example/music/user/dto/LoginRequestDto.java
+++ src/main/java/com/example/music/user/dto/LoginRequestDto.java
@@ -8,7 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LoginRequestDto {
 
-    @Pattern(regexp = "^[a-zA-Z0-9]$", message = "최소 4자 이상, 10자 이하이며 알파벳 소문자(a~z), 숫자(0~9)")
+//    @Pattern(regexp = "^[a-zA-Z0-9].{10,25}$")
+    @Pattern(regexp = "^[a-zA-Z0-9]+@[a-zA-Z0-9]{4,6}\\.[a-zA-Z]{2,5}$")
     private String username;
 
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=!])(?!.*\\s).{8,15}$", message = "최소 8자이상, 15자 이하이며 알파벳 소문자, 대문자, 숫자 (0~9) 하나 필수, 문자(@#$%^&+=!) 하나 필수")
